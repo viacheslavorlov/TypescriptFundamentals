999afd81fda102fd2038ab7696284c2de7f99d3c
viacheslavorlov <viacheslavorlov@gmail.com>
2022-07-28 23:42:35 +0300

    часть 1 типы данных 18 минут (https://www.youtube.com/watch?v=nyIpDs2DJ_c)

--- 1types.ts
+++ 1types.ts
@@ -1,5 +1,44 @@
 const isFetching: boolean = true;
 const isLoading: boolean = false;
 
-let int: number = 42;
+//* числа
+const int: number = 42;
 //! int = '';  нельзя менять тип данных переменной!!!!!
+const float: number = 4.2;
+const num: number = 3e10;
+
+//* string
+const message: string = 'Hello Typescript!';
+
+//*Array
+const numArray: number[] = [1, 1, 2, 3, 8, 13];
+const numArray2: Array<number> = [1, 1, 2, 3, 8, 13];
+const wordArray: string[] = ['Hello', 'Typescript!'];
+const wordArray2: Array<string> = ['Hello', 'Typescript!'];
+
+//*Tuple
+const contact: [string, number] = ['slava', 12345];
+
+//*/Any - любой тип данных
+let variable: any = 42;
+variable = 'new string'; //? может менять тип данных
+variable = [1, 2, 4];  //? может менять тип данных
+
+//todo =====================
+
+function sayMyName(name: string): void {
+    console.log(name);
+}
+
+sayMyName('o, hi Mark!');
+
+//* Never
+function throwError(message: string): never {
+    throw new Error(message);
+}
+
+function infinite(): never {
+    while (true) {
+        console.log('infinite!');
+    }
+}
