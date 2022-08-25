40fd57955f881db1c5a48b68be06867dbb3a1b8e
viacheslavorlov <viacheslavorlov@gmail.com>
2022-08-25 16:10:53 +0300

    часть 5 classes (https://www.youtube.com/watch?v=nyIpDs2DJ_c)

--- 5_classes.ts
+++ 5_classes.ts
@@ -13,6 +13,41 @@ class Car {
     readonly model: string
     readonly numberOfWheels: number = 4
 
-    constructor() {
+    constructor(theModel, wheals) {
+        this.model = theModel
+        this.numberOfWheels = wheals
+    }
+
+    info() {
+        return `Car ${this.model} has ${this.numberOfWheels} wheals`;
     }
 }
+const typescript =  new Typescript('4.7.4');
+const lada = new Car('лада', 4)
+
+console.log(typescript.info('project'));
+console.log(lada.info());
+
+
+//* ====================== *//
+
+class Animal {
+    protected voice: string = ''
+    public color: string = 'black'
+
+    private go() {
+        console.log('Go!')
+    }
+}
+
+class Cat extends Animal {
+    public setVoice(voice: string): void {
+        this.voice = voice;
+    }
+}
+
+const cat = new Cat();
+
+cat.setVoice('Miu...');
+console.log(cat.color);
+//* cat.voice; cat.go()  //так нельзя - приватное свойство
